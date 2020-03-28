056c3cc3b6c51ef8d2105e136b5e03e846596d78
Roman James Mirov <romanjamesmirov@gmail.com>
2020-03-28 16:33:38 -0700

    Basic functionality down I think

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,31 +1,71 @@
 import React, { Component } from 'react'
 
+import Plus from '../static/Plus.svg'
+
 export default class Tasks extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			activeTasks: [],
+			inactiveTasks: []
+		}
+		this.createTask = this.createTask.bind(this)
+		this.toggleTask = this.toggleTask.bind(this)
+	}
+
+	createTask() {
+		const newTaskName = prompt('Task')
+		this.setState(state => ({ inactiveTasks: [...state.inactiveTasks, newTaskName] }))
+	}
+
+	toggleTask(e) {
+		let newActiveTasks = [...this.state.activeTasks]
+		let newInactiveTasks = [...this.state.inactiveTasks]
+		const toggledTask = e.target.textContent
+		if (newActiveTasks.indexOf(toggledTask) !== -1) {
+			newActiveTasks.splice(newActiveTasks.indexOf(toggledTask), 1)
+			newInactiveTasks.unshift(toggledTask)
+		} else if (newInactiveTasks.indexOf(toggledTask) !== -1) {
+			newInactiveTasks.splice(newInactiveTasks.indexOf(toggledTask), 1)
+			newActiveTasks.unshift(toggledTask)
+		}
+		this.setState(state => ({
+			activeTasks: [...newActiveTasks],
+			inactiveTasks: [...newInactiveTasks]
+		}))
+	}
+
 	render() {
 		return (
 			<div className='Tasks-Page'>
 				<section className='Active-Tasks'>
 					<ul>
-						<li>Coding</li>
-						<li>Jazz</li>
+						{this.state.activeTasks.length === 0
+							? <li className='Empty-Tasks-Message'>You have no active tasks.</li>
+							: this.state.activeTasks.map((task, index) => (
+								<li key={index} className='Task-Item'>
+									<button onClick={this.toggleTask}>{task}</button>
+								</li>
+							))}
 					</ul>
 				</section>
 				<hr />
 				<section className='Inactive-Tasks'>
 					<ul>
-						<li>Cooking</li>
-						<li>Walking</li>
-						<li>Sleep</li>
-						<li>Dad</li>
-						<li>Yoga</li>
-						<li>Push/pull weight</li>
-						<li>Pulling roots</li>
-						<li>Games</li>
-						<li>Porn</li>
-						<li>Youtube</li>
-						<li>Chatrooms</li>
-						<li>Job search</li>
-						<li>Friends and fam</li>
+						{this.state.activeTasks.length === 0 && this.state.inactiveTasks.length === 0
+							? <li className='Empty-Tasks-Message'>Add a new task ➡️</li>
+							: this.state.inactiveTasks.length === 0
+								? <li className='Empty-Tasks-Message'>All tasks are active.</li>
+								: this.state.inactiveTasks.map((task, index) => (
+									<li key={index} className='Task-Item'>
+										<button onClick={this.toggleTask}>{task}</button>
+									</li>
+								))}
+						<li className='Create-Task'>
+							<button onClick={this.createTask}>
+								<img src={Plus} alt='Plus icon' />
+							</button>
+						</li>
 					</ul>
 				</section>
 			</div>
