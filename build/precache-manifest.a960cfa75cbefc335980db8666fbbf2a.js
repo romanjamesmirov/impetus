c7c60d5b72a007b0ef1a8d77edfd484ec5cfd0a1
Roman James Mirov <romanjamesmirov@gmail.com>
2020-03-28 19:13:15 -0700

    Added Electron so it's ready for Heroku

---
+++ build/precache-manifest.a960cfa75cbefc335980db8666fbbf2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a1bf38d5fadcbe9bbdd168d2f03a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9712f1864c723d7cde56",
+    "url": "/static/css/main.9f153467.chunk.css"
+  },
+  {
+    "revision": "91ee0dc6fd60590f84f4",
+    "url": "/static/js/2.03328a0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.03328a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9712f1864c723d7cde56",
+    "url": "/static/js/main.3b946a26.chunk.js"
+  },
+  {
+    "revision": "80d06b31ff42db2c626d",
+    "url": "/static/js/runtime-main.20620456.js"
+  },
+  {
+    "revision": "e105d9ca12d49c968120cca159b52de6",
+    "url": "/static/media/Plus.e105d9ca.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
